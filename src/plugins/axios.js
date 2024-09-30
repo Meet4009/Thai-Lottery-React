aae8996034efe4b718fae15db6f41caad008c54e
Meet4009 <meetsolanki4009@gmail.com>
2024-10-01 01:36:36 +0530

    change base Url2

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customAxios = axios.create({
   // baseURL: "https://thailottery.onrender.com/api",
-  baseURL: "https://thailottery.cloud",
+  baseURL: "https://thailottery.cloud/api",
 });
 
 const requestHandler = (request) => {
